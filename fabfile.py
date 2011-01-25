eb5f7fdc79cd255f7f9e2cefba7f4f62a41939ad
Mikkel Hoegh <mikkel@hoegh.org>
2011-01-25 08:46:31 +0100

    More generalised fabfile.

--- fabfile.py
+++ fabfile.py
@@ -16,11 +16,45 @@ env.roledefs = {
 }
 env.user = 'fabric'
 
+
+def update(commit_id):
+    """
+    Update the install profile without creating a new copy.
+
+    Usage: "fab -R staging update:113693f" where 113693f is the Git commit id.
+
+    Beware that this action can be dangerous to run on a new site, but it is a
+    lot faster than building a new install profile from scratch.
+    Be aware that this will only update the profile working copy, not the
+    drush make file.
+    """
+    commit_id = commit_id.strip()
+    if (len(commit_id) < 7 or len(commit_id) > 40):
+        abort('Please specify a commit id between 7 or 40 characters.')
+
+    try:
+        float.fromhex(commit_id)
+    except ValueError:
+        abort('Commit ID must be a hexadecimal number (0-9a-f).')
+
+    require('roles', 'hosts', used_for="configuring what servers to deploy to.")
+
+    with cd('{path}/{profile}'.format(path=PROFILES_PATH, profile=PROFILE_NAME)):
+        run('git fetch')
+        run('git checkout {commit}'.format(commit=commit_id))
+
+    post_deploy()
+
 def deploy(commit_id):
     """
     Deploy a specific Git version to the server.
 
     Usage: "fab -R staging deploy:113693f" where 113693f is the Git commit id.
+
+    This command will make a new checkout of the profile repository, build
+    the profile via its make file, replace the previous deployed version
+    with the new build, and finally run the post deploy actions, upgrading
+    the database, etc.
     """
     commit_id = commit_id.strip()
     if (len(commit_id) < 7 or len(commit_id) > 40):
@@ -42,13 +76,14 @@ def deploy(commit_id):
         run('git clone {repo} {profile}.new'.format(repo=GIT_REPO, profile=PROFILE_NAME))
 
         # Checkout the desired commit ID and run the make file.
-        with cd('vopros.new'):
+        with cd('{profile}.new'.format(profile=PROFILE_NAME)):
             run('git checkout {commit}'.format(commit=commit_id))
             run('{drush} -y make --no-core --contrib-destination=. {profile}.make'.format(drush=DRUSH_PATH, profile=PROFILE_NAME))
 
         # Move the current version of the code to .old.
-        run('rm -r {profile}.old'.format(profile=PROFILE_NAME))
-        run('mv {profile} {profile}.old'.format(profile=PROFILE_NAME))
+        with settings(warn_only=True):
+            run('rm -r {profile}.old'.format(profile=PROFILE_NAME))
+            run('mv {profile} {profile}.old'.format(profile=PROFILE_NAME))
 
         # Put the new copy into production.
         run('mv {profile}.new {profile}'.format(profile=PROFILE_NAME))
@@ -90,4 +125,5 @@ def rollback():
         run('mv {profile}.old {profile}'.format(profile=PROFILE_NAME))
 
     post_deploy()
+
 
